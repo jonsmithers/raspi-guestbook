866704d95f21dc9ae5ab984e0d863cfbe20061b8
Jon Smithers <mail@jonsmithers.link>
2017-08-13 18:04:27 -0400

    Accumulate data

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 global.ON_RASPBERRY = false;
 global.LOGFILE      = global.ON_RASPBERRY ? '/var/log/auth.log' : './auth.log';
 
+const server = require('./server');
 const logWatcher = require('./log-watcher');
 
 
 global.run = () => {
-  require('./server').start();
-  logWatcher.observable.subscribe(({date, ip, user}) => {
-    console.log(user, ip, new Date(date).toLocaleString());
-  }, console.error);
-  console.log('subscriber created');
-
   if (!global.ON_RASPBERRY) {
     console.log('(starting test script)');
     logWatcher.runTestScript().then(() =>
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-module.exports = {
-  start() {
-    const Hapi = require('hapi');
-    const server = new Hapi.Server();
-    server.connection({host: 'localhost', port: '8000'});
-    server.route({
-      method: 'GET',
-      path:'/',
-      handler: function (request, reply) {
-        return reply('hello world');
-      }
-    });
-    server.start(err => {
-      if (err) {
-        throw err;
-      }
-      console.log('Server running at:', server.info.uri);
-    });
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+const accumulatedData = [];
 
+server.connection({port: '8000'});
+server.route({
+  method: 'GET',
+  path:'/',
+  handler: function (request, reply) {
+    return reply(accumulatedData);
   }
-};
+});
+server.start(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
+
+
+
+const logWatcher = require('./log-watcher');
+logWatcher.observable.subscribe((object) => {
+  accumulatedData.push(object);
+  console.log(object);
+}, console.error);
